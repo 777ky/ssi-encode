8ea39e95a5ca179945365d7d359b3bece30a98fe
opdykurihara <ykurihara@opendoor.co.jp>
2018-06-06 12:15:02 +0900

    First commit!

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const fs = require('fs')
+const path = require('path')
+const jschardet = require('jschardet')
+const iconvLite = require('iconv-lite')
+const SSI  = require("node-ssi");
+const defaults = require('defaults');
+
+module.exports = function SSIencode(option){
+  option = defaults(option,{
+    baseDir: ".",
+    encoding: "binary",
+    ext: ".html"
+  })
+  const ssi = new SSI(option)
+
+  return function(req, res, next) {
+    const url = req.url.slice(-1) === "/" ? req.url + "index" + option.ext : req.url
+    const reg = new RegExp("\."+option.ext+"$")
+
+    if (!reg.test(url)) return next()
+
+    ssi.compileFile(path.join(option.baseDir, url), function(err, content){
+
+      if (err) return next(err)
+
+      content = iconvLite.decode(new Buffer.from(content, "binary"), jschardet.detect(content).encoding );
+
+      res.setHeader("Content-Type", "text/html; charset=UTF-8")
+      res.end(content)
+    });
+  }
+
+}
