d9997f9f917aebd4f5a707e0c31f03eec8ba976d
opdykurihara <ykurihara@opendoor.co.jp>
2018-06-06 18:36:28 +0900

    ssi-encod
    node-ssiをforkしてroutesオプションが利用できるようにした。

--- index.js
+++ index.js
@@ -4,32 +4,33 @@ const fs = require('fs')
 const path = require('path')
 const jschardet = require('jschardet')
 const iconvLite = require('iconv-lite')
-const SSI  = require("node-ssi");
+// const SSI  = require("node-ssi");
+const SSI  = require("./fork/node-ssi");
 const defaults = require('defaults');
 
 module.exports = function SSIencode(option){
   option = defaults(option,{
     baseDir: ".",
+    routes: {},
     encoding: "binary",
     ext: ".html"
   })
   const ssi = new SSI(option)
-
-  return function(req, res, next) {
+  return function(req, res, next){
     const url = req.url.slice(-1) === "/" ? req.url + "index" + option.ext : req.url
     const reg = new RegExp("\."+option.ext+"$")
 
     if (!reg.test(url)) return next()
 
-    ssi.compileFile(path.join(option.baseDir, url), function(err, content){
+    const filepath = path.join(option.baseDir, url);
 
+    ssi.compileFile(filepath, option, function(err, content){
       if (err) return next(err)
-
       content = iconvLite.decode(new Buffer.from(content, "binary"), jschardet.detect(content).encoding );
-
       res.setHeader("Content-Type", "text/html; charset=UTF-8")
       res.end(content)
     });
+
   }
 
 }
